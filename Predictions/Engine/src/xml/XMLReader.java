da22793fe0e68126320ed6cc8c8ee897a6103825
Shiraz Yom Tov <shirazyomtov@gmail.com>
2023-08-06 08:31:00 +0300

    Update the files related to reading and validation of XML file
    The changes made:
    
    1. Separated between reading the file and checking for correctness
    
    2.Update the functions: checkRuleProperty(), checkArgumentThatGivenToCalculationIncreaseDecrease();
    
    3.Added tests for the user's path
    
    4.Update the enum AuxiliaryFunction

--- Predictions/Engine/src/xml/XMLReader.java
+++ Predictions/Engine/src/xml/XMLReader.java
@@ -1,105 +1,22 @@
 package xml;
 
-import exceptions.NameAlreadyExist;
-import exceptions.ObjectNotExist;
 import jaxb.schema.generated.*;
-
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.*;
-import java.util.HashMap;
-
-import static java.lang.Float.parseFloat;
-import static world.enums.AuxiliaryFunction.checkOptionByFunctionName;
-
 
 public final class XMLReader {
-    //maybe static
-    private static String xmlPath = "src/resources/check/master-ex1.xml";
-
     private final static String JAXB_XML_GAME_PACKAGE_NAME = "jaxb.schema.generated";
-    private static PRDWorld world = null;
 
-    public XMLReader() {
-    }
-
-    public static void setWorld(PRDWorld world) {
-        XMLReader.world = world;
-    }
-
-    public static void openXmlAndGetData() throws FileNotFoundException, JAXBException {
+    public static void openXmlAndGetData(String xmlPath) throws FileNotFoundException, JAXBException {
         InputStream inputStream = new FileInputStream(new File(xmlPath));
-        setWorld(deserializeFrom(inputStream));
+        XMLValidation.setWorld(deserializeFrom(inputStream));
     }
 
     private static PRDWorld deserializeFrom(InputStream inputStream) throws JAXBException {
         JAXBContext jc = JAXBContext.newInstance(JAXB_XML_GAME_PACKAGE_NAME);
         Unmarshaller u = jc.createUnmarshaller();
         return (PRDWorld) u.unmarshal(inputStream);
-    }
-
-    public static void checkValidationXmlFile() throws NameAlreadyExist, ObjectNotExist {
-        checkEnvironmentVariables();
-        checkPropertyVariablesOfSpecificEntity(world.getPRDEntities().getPRDEntity().get(0));
-        checkActionsEntitiesNames();
-        checkArgumentThatGivenToCalculationIncreaseDecrease();
-    }
-
-    private static void checkEnvironmentVariables() throws NameAlreadyExist {
-        HashMap<String, Integer> map = new HashMap<>();
-        for (PRDEnvProperty prdEnvironment : world.getPRDEvironment().getPRDEnvProperty()) {
-            if (map.containsKey(prdEnvironment.getPRDName())) {
-                throw new NameAlreadyExist((prdEnvironment.getPRDName()), "enviroment");
-            } else {
-                map.put(prdEnvironment.getPRDName(), 1);
-            }
-        }
-    }
-
-    private static void checkPropertyVariablesOfSpecificEntity(PRDEntity prdEntity) throws NameAlreadyExist {
-        HashMap<String, Integer> map = new HashMap<>();
-        for (PRDProperty prdProperty : prdEntity.getPRDProperties().getPRDProperty()) {
-            if (map.containsKey(prdProperty.getPRDName())) {
-                throw new NameAlreadyExist((prdProperty.getPRDName()), "property");
-            } else {
-                map.put(prdProperty.getPRDName(), 1);
-            }
-        }
-    }
-
-    private static void checkActionsEntitiesNames() throws ObjectNotExist {
-        for (PRDRule rule : world.getPRDRules().getPRDRule()) {
-            for (PRDAction action : rule.getPRDActions().getPRDAction()) {
-                if (!action.getEntity().equals(world.getPRDEntities().getPRDEntity().get(0).getName())) {
-                    throw new ObjectNotExist(action.getEntity(), "Entity");
-                }
-            }
-
-        }
-
-    }
-
-    private static void checkArgumentThatGivenToCalculationIncreaseDecrease() throws
-    {
-        for (PRDRule rule : world.getPRDRules().getPRDRule()) {
-            for (PRDAction action : rule.getPRDActions().getPRDAction()) {
-               if(action.getType().equals("decrease") || action.getType().equals("increase")){
-                   if(checkOptionByFunctionName(action.getBy())){
-
-                   }
-
-                   try
-                   {
-                        parseFloat(action.getBy());
-                   }
-                   catch(NumberFormatException e){
-                       throw new
-                   }
-               }
-            }
-
-        }
-
     }
 }
---
+++ Predictions/Engine/src/xml/XMLValidation.java
@@ -0,0 +1,211 @@
+package xml;
+
+import exceptions.NameAlreadyExist;
+import exceptions.ObjectNotExist;
+import jaxb.schema.generated.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static world.enums.AuxiliaryFunction.checkOptionByFunctionName;
+
+public class XMLValidation {
+    private static PRDWorld world = null;
+
+    public static void setWorld(PRDWorld world) {
+        XMLValidation.world = world;
+    }
+
+    public static void checkValidationXmlFile() throws NameAlreadyExist, ObjectNotExist, NumberFormatException {
+        checkEnvironmentVariables();
+        checkPropertyVariablesOfSpecificEntity(world.getPRDEntities().getPRDEntity().get(0));
+        checkActionsEntitiesNames();
+        checkRuleProperty();
+        checkArgumentThatGivenToCalculationIncreaseDecrease();
+    }
+
+    private static void checkEnvironmentVariables() throws NameAlreadyExist {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (PRDEnvProperty prdEnvironment : world.getPRDEvironment().getPRDEnvProperty()) {
+            if (map.containsKey(prdEnvironment.getPRDName())) {
+                throw new NameAlreadyExist((prdEnvironment.getPRDName()), "environment");
+            } else {
+                map.put(prdEnvironment.getPRDName(), 1);
+            }
+        }
+    }
+
+    private static void checkPropertyVariablesOfSpecificEntity(PRDEntity prdEntity) throws NameAlreadyExist {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (PRDProperty prdProperty : prdEntity.getPRDProperties().getPRDProperty()) {
+            if (map.containsKey(prdProperty.getPRDName())) {
+                throw new NameAlreadyExist((prdProperty.getPRDName()), "property");
+            } else {
+                map.put(prdProperty.getPRDName(), 1);
+            }
+        }
+    }
+
+    private static void checkActionsEntitiesNames() throws ObjectNotExist {
+        for (PRDRule rule : world.getPRDRules().getPRDRule()) {
+            for (PRDAction action : rule.getPRDActions().getPRDAction()) {
+                if (!action.getEntity().equals(world.getPRDEntities().getPRDEntity().get(0).getName())) {
+                    throw new ObjectNotExist(action.getEntity(), "Entity");
+                }
+            }
+        }
+    }
+
+    private static void checkRuleProperty() throws ObjectNotExist {
+        for(PRDRule rule: world.getPRDRules().getPRDRule()){
+            for(PRDAction action: rule.getPRDActions().getPRDAction()) {
+                String entityAction = action.getEntity();
+                if (action.getType().equals("condition")){
+                    List<PRDCondition> conditions = action.getPRDCondition().getPRDCondition();
+                    if (action.getPRDCondition().getSingularity().equals("single")) {
+                        findProperty(action.getPRDCondition().getProperty(), entityAction);
+                    }
+                    else{
+                        checkCondition(entityAction, conditions);
+                    }
+                }
+                else if(action.getType().equals("calculation")){
+                    findProperty(action.getResultProp(), entityAction);
+                }
+                else{
+                    findProperty(action.getProperty(), entityAction);
+                }
+            }
+        }
+    }
+
+    private static void checkCondition(String entityAction, List<PRDCondition> conditions) throws ObjectNotExist {
+        if (conditions.equals("single")) {
+            findProperty(conditions.get(0).getProperty(), entityAction);
+        }
+        else{
+            for(PRDCondition condition: conditions){
+                if (condition.getSingularity().equals("multiple")) {
+                    checkCondition(entityAction, condition.getPRDCondition());
+                }
+                else{
+                    findProperty(conditions.get(0).getProperty(), entityAction);
+
+                }
+            }
+        }
+    }
+
+    private static void findProperty(String propertyName, String entityAction) throws ObjectNotExist {
+        boolean flag = false;
+        for(PRDEntity entity: world.getPRDEntities().getPRDEntity()){
+            if(entityAction.equals(entity.getName())){
+                for(PRDProperty property: entity.getPRDProperties().getPRDProperty()){
+                    if(property.getPRDName().equals(propertyName)){
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+        }
+        if(!flag){
+            throw new ObjectNotExist(propertyName, "property");
+        }
+    }
+
+    private static void checkArgumentThatGivenToCalculationIncreaseDecrease() throws NumberFormatException
+    {
+        for (PRDRule rule : world.getPRDRules().getPRDRule()) {
+            for (PRDAction action : rule.getPRDActions().getPRDAction()) {
+                String entityName = action.getEntity();
+                if(action.getType().equals("decrease") || action.getType().equals("increase")){
+                    String by = action.getBy();
+                    checkTypeOfArg(by, entityName);
+                }
+                else if(action.getType().equals("calculation")){
+                    if(!checkTypeOfEntityProperty(action.getResultProp(), action.getEntity())){
+                        throw new NumberFormatException("Not a number: " + action.getBy());
+                    }
+                    if(action.getPRDDivide() != null){
+                        checkTypeOfArg(action.getPRDDivide().getArg1(), entityName);
+                        checkTypeOfArg(action.getPRDDivide().getArg2(), entityName);
+                    }
+                    else{
+                        checkTypeOfArg(action.getPRDMultiply().getArg1(), entityName);
+                        checkTypeOfArg(action.getPRDMultiply().getArg2(), entityName);
+                    }
+                }
+            }
+        }
+    }
+
+    private static void checkTypeOfArg(String arg, String entityName){
+        if(checkOptionByFunctionName(arg)){
+            String value = extractValueInParentheses(arg);
+            if(!checkTypeOfEnvironmentProperty(value)){
+                throw new NumberFormatException("Not a number: " + value);
+            }
+        }
+        else{
+            if(!checkTypeOfEnvironmentProperty(arg) && !checkTypeOfEntityProperty(arg, entityName)){
+                throw new NumberFormatException("Not a number: " + arg);
+            }
+        }
+    }
+
+    private static String extractValueInParentheses(String input) {
+        Pattern pattern = Pattern.compile("\\((.*?)\\)");
+        Matcher matcher = pattern.matcher(input);
+
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+
+        return null;
+    }
+
+    private static boolean checkTypeOfEnvironmentProperty(String value) {
+        boolean flag = false;
+        if(isFloat(value)){
+            flag = true;
+        }
+        else {
+            for (PRDEnvProperty envProperty : world.getPRDEvironment().getPRDEnvProperty()) {
+                if (envProperty.getPRDName().equals(value)) {
+                    if (envProperty.getType().equals("decimal") || envProperty.getType().equals("float")) {
+                        flag = true;
+                    }
+                }
+            }
+        }
+
+        return flag;
+    }
+
+    private static boolean checkTypeOfEntityProperty(String value, String entityName) {
+        boolean flag = false;
+        for (PRDEntity entity : world.getPRDEntities().getPRDEntity()) {
+            if (entity.getName().equals(entityName)) {
+                for(PRDProperty property: entity.getPRDProperties().getPRDProperty()) {
+                    if(property.getPRDName().equals(value)) {
+                        if (property.getType().equals("decimal") || property.getType().equals("float")) {
+                            flag = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return flag;
+    }
+
+    private static boolean isFloat(String str) {
+        try {
+            Float.parseFloat(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
